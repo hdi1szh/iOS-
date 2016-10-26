1915c6433695a026a0bec10fbf8f3a7cac45a8eb
黄定师 <huangdingshi@bogon>
2016-10-26 14:21:03 +0800

    Initial Commit

---
+++ iOS适配/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS适配
+//
+//  Created by 黄定师 on 16/10/26.
+//  Copyright © 2016年 黄定师. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS适配/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS适配
+//
+//  Created by 黄定师 on 16/10/26.
+//  Copyright © 2016年 黄定师. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
